4b47cfeab8838e1b680d4a7bd9929cefb01bdec6
tang-crush-main <leotranngunguoi128@gmail.com>
2021-08-20 18:21:35 +0700

    Quốc Bình

---
+++ js/config 1.jsx
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello bạn Quốc Bình !',
+    introDesc: `Tôi có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Chơii',
+    title: 'Cậu còn yêu Duyên có phải không ?',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu còn yêu đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'cậu bị ảo à :333',
+    question:'Trên thế giới có 7 tỉ người mà sao cậu vẫn yêu Duyên ?',
+    btnReply: 'Gửi cho tôi đi <3',
+    reply: 'Tại vì tôi là một thằng lụy U_U U_U',
+    mess: 'Tôi biết mà !. Cậu là một thằng lụy !',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không nói cho Duyên đi nào.',
+    btnAccept: 'Okiiiii lun :>',
+    messLink: 'https://www.facebook.com/nam.pe.7547' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
